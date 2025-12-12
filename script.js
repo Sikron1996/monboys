2e348d534da37e1dc629f6d7151a800789f7012d
Sikron1996 <vadimmonarh@gmail.com>
2025-12-12 16:03:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-// reveal on scroll
-const sections = document.querySelectorAll('.section');
-const io = new IntersectionObserver(entries => {
-  entries.forEach(e => {
-    if (e.isIntersecting) {
-      e.target.style.opacity = 1;
-      e.target.style.transform = 'none';
-    }
-  });
-}, { threshold: 0.4 });
-
-sections.forEach(s => {
-  s.style.opacity = 0;
-  s.style.transform = 'translateY(30px)';
-  s.style.transition = '1s';
-  io.observe(s);
+const cursor=document.querySelector('.cursor');
+window.addEventListener('mousemove',e=>{
+cursor.style.left=e.clientX+'px';
+cursor.style.top=e.clientY+'px';
+});
+const sections=document.querySelectorAll('.section');
+const io=new IntersectionObserver(entries=>{
+entries.forEach(e=>{
+if(e.isIntersecting){
+e.target.style.opacity=1;
+e.target.style.transform='none';
+}
+})
+},{threshold:.35});
+sections.forEach(s=>{
+s.style.opacity=0;
+s.style.transform='translateY(30px)';
+s.style.transition='1s';
+io.observe(s);
 });
